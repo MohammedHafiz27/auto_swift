a325f73ed904d71269e31584ba7535533d363c91
mohammed hafiz <mohammed.hafiz.2710@gmail.com>
2025-09-15 12:19:22 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:auto_swift/Features/admin_page/presentation/views/admin_page_view.dart';
+import 'package:auto_swift/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
